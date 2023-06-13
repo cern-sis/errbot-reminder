06393e4639b14886a9186b53c6b6af6518bce876
adrien5054 <ad.stucki@free.fr>
2023-06-13 15:42:20 +0200

    gh-action: test for reminder.py
    * errbot added to the .toml file

--- reminder.py
+++ reminder.py
@@ -1,82 +1,105 @@
-from datetime import date, datetime, time, timedelta
+from datetime import date, datetime, time, timedelta, timezone
 
 import pytz
 from errbot import BotPlugin, botcmd
 
-timezone = pytz.timezone("Europe/Zurich")
-
 # Daily / Retrospective --> If it's an ordinary day or a retrospective, meeting at 9:30
 # Sprint Planning --> If it's a Sprint planning, meeting at 15:30
 # Sprint review --> If it's a Sprint Review, meeting at 14:45
+
 #  -----------------
+
+utc = pytz.timezone("UTC")
+now = utc.localize(datetime.utcnow())
+tz_cern = pytz.timezone("Europe/Zurich")
+now.astimezone(tz_cern)
 
 
 class Reminder(BotPlugin):
-    def get_monday(self, today):
+    @staticmethod
+    def get_monday(today):
         today_weekday = today.weekday()
         monday = date.today() - timedelta(days=today_weekday)
         monday = datetime.combine(monday, datetime.min.time())
         return monday.date()
 
-    def is_sprint_planning(self):
-        first_iteration_startdate = datetime(2022, 2, 28)
-        nb_days = first_iteration_startdate - self.get_monday(date.today())
-        nb_weeks = nb_days / 7
-        nb_weeks = int(nb_weeks.total_seconds())
+    @staticmethod
+    def is_sprint_planning():
+        first_iteration_startdate = date(2022, 2, 28)
+        nb_days = (first_iteration_startdate - Reminder.get_monday(date.today())).days
+        nb_weeks = nb_days // 7
 
         return nb_weeks % 2 == 0
 
     EVENTS = {
-        "Sprint planning": (datetime(2022, 2, 28, 15, 30), timedelta(weeks=2)),
-        "Sprint review": (datetime(2022, 3, 10, 14, 45), timedelta(weeks=2)),
-        "Sprint restrospective": (datetime(2022, 3, 11, 9, 30), timedelta(weeks=2)),
-        "Daily meeting": (datetime(2022, 3, 1, 9, 30), timedelta(days=1)),
+        "Sprint planning": (
+            tz_cern.localize(datetime(2022, 2, 28, 15, 30)),
+            timedelta(weeks=2),
+        ),
+        "Sprint review": (
+            tz_cern.localize(datetime(2022, 3, 10, 14, 45)),
+            timedelta(weeks=2),
+        ),
+        "Sprint retrospective": (
+            tz_cern.localize(datetime(2022, 3, 11, 9, 30)),
+            timedelta(weeks=2),
+        ),
+        "Daily meeting": (
+            tz_cern.localize(datetime(2022, 3, 1, 9, 30)),
+            timedelta(days=1),
+        ),
     }
 
-    def next_occurance(self, meeting):
-        today = datetime.now(timezone)
-        next_occurance = self.EVENTS.get(meeting)[0]
-        delta_occurance = self.EVENTS.get(meeting)[1]
+    @staticmethod
+    def next_occurance(meeting, today):
+        today = tz_cern.localize(today)
+        next_occurance = Reminder.EVENTS.get(meeting)[0].astimezone(tz_cern)
+        delta_occurance = Reminder.EVENTS.get(meeting)[1]
 
         while next_occurance <= today:
             next_occurance += delta_occurance
 
         if next_occurance > today:
-            return next_occurance
+            return next_occurance.strftime("%Y-%m-%d %H:%M")
 
-    def next_daily(self, today):
-        next_daily = self.EVENTS.get("Daily meeting")[0]
-        delta_daily = self.EVENTS.get("Daily meeting")[1]
+    @staticmethod
+    def next_daily(today):
+        today = tz_cern.localize(today)
+        next_daily = Reminder.EVENTS.get("Daily meeting")[0].astimezone(tz_cern)
+        delta_daily = Reminder.EVENTS.get("Daily meeting")[1]
 
         while next_daily <= today:
             next_daily += delta_daily
 
-        if self.is_sprint_planning():
-            if next_daily.weekday() == 0:
-                next_daily += delta_daily
+            if Reminder.is_sprint_planning():
+                if next_daily.weekday() == 0:
+                    next_daily += delta_daily
 
-            if next_daily.weekday() >= 5:
-                next_daily += timedelta(days=(7 - next_daily.weekday()))
+                if next_daily.weekday() >= 5:
+                    next_daily += timedelta(days=(7 - next_daily.weekday()))
 
-        else:
-            if next_daily.weekday() >= 3:
-                next_daily += timedelta(days=(7 - next_daily.weekday() + 1))
+            else:
+                if next_daily.weekday() >= 3:
+                    next_daily += timedelta(days=(7 - next_daily.weekday() + 1))
 
-        return next_daily
+        return next_daily.strftime("%Y-%m-%d %H:%M")
 
     @botcmd
-    def reminder_next(self, msg, args):
+    def reminder_next():
+        today = datetime.now()
+        next_retrospective = Reminder.next_occurance("Sprint retrospective", today)
         return "\n".join(
-            f"Next planning: {self.next_occurance('Sprint planning')}",
-            f"Next daily: {self.next_daily(datetime.now(timezone))}",
-            f"Next review: {self.next_occurance('Sprint review')}"
-            f"Next retrospective: {self.next_occurance('Sprint restrospective')}",
+            [
+                f"Next planning:{Reminder.next_occurance('Sprint planning', today)}",
+                f"Next daily: {Reminder.next_daily(today)}",
+                f"Next review: {Reminder.next_occurance('Sprint review', today)}",
+                f"Next retrospective: {next_retrospective}",
+            ]
         )
 
     @botcmd
     def notify_for_daily_meeting(self, msg, args):
         stream = msg._from._room._id
-        topic = msg._from._room._subject
 
         client = self._bot.client
 
---
+++ tests/test_reminders.py
@@ -0,0 +1,66 @@
+import os
+import sys
+from datetime import date, datetime
+
+from freezegun import freeze_time
+
+from reminder import Reminder
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+
+
+@freeze_time("2023-06-15")
+def test_get_monday():
+    test_date = date(2023, 6, 15)
+    assert Reminder.get_monday(test_date) == date(2023, 6, 12)
+
+
+@freeze_time("2023-06-12")
+def test_is_spring_planning():
+    assert not Reminder.is_sprint_planning()
+
+
+@freeze_time("2023-06-12")
+def test_next_planning():
+    test_date = datetime(2023, 6, 12, 15, 32)
+    expected_result = datetime(2023, 6, 19, 15, 30).strftime("%Y-%m-%d %H:%M")
+
+    assert Reminder.next_occurance("Sprint planning", test_date) == expected_result
+
+
+@freeze_time("2023-06-12")
+def test_next_review():
+    test_date = datetime(2023, 6, 12, 15, 32)
+    expected_result = datetime(2023, 6, 15, 14, 45).strftime("%Y-%m-%d %H:%M")
+
+    assert Reminder.next_occurance("Sprint review", test_date) == expected_result
+
+
+@freeze_time("2023-06-12")
+def test_next_retrospective():
+    test_date = datetime(2023, 6, 12, 15, 32)
+    expected_result = datetime(2023, 6, 16, 9, 30).strftime("%Y-%m-%d %H:%M")
+
+    assert Reminder.next_occurance("Sprint retrospective", test_date) == expected_result
+
+
+@freeze_time("2023-06-12 15:32", tz_offset=2)
+def test_next_daily():
+    test_date = datetime(2023, 6, 12, 15, 32)
+    expected_result = datetime(2023, 6, 13, 9, 30).strftime("%Y-%m-%d %H:%M")
+    assert Reminder.next_daily(test_date) == expected_result
+
+
+@freeze_time("2023-06-12")
+def test_reminder_next():
+    restrospective_content = datetime(2023, 6, 16, 9, 30)
+    expected_response = "\n".join(
+        [
+            f"Next planning:{datetime(2023, 6, 19, 15, 30).strftime('%Y-%m-%d %H:%M')}",
+            f"Next daily: {datetime(2023, 6, 13, 9, 30).strftime('%Y-%m-%d %H:%M')}",
+            f"Next review: {datetime(2023, 6, 15, 14, 45).strftime('%Y-%m-%d %H:%M')}",
+            f"Next retrospective: {restrospective_content.strftime('%Y-%m-%d %H:%M')}",
+        ]
+    )
+    assert Reminder.reminder_next() == expected_response
