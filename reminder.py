ac7ff87d9f38059346b4721b05db494e68ad9e29
adrien5054 <ad.stucki@free.fr>
2023-06-19 16:00:43 +0200

    gh-action: error correction
    * Automation of 'notify_for_daily_meetings'

--- reminder.py
+++ reminder.py
@@ -158,4 +158,5 @@ class Reminder(BotPlugin):
 
     def activate(self):
         super().activate()
-        self.start_poller(60, self.notify_for_daily_meeting())
+        self.start_poller(60, self.notify_for_daily_meeting(None, None, None))
+        return "Daily meetings notifications activated"
