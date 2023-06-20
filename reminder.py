af350c1ab6094c1696150e60e9b6746933750671
adrien5054 <ad.stucki@free.fr>
2023-06-20 15:08:39 +0200

    reminder: create automatic function
    * Test of time operations

--- reminder.py
+++ reminder.py
@@ -119,12 +119,13 @@ class Reminder(BotPlugin):
                 if next_occurance > today:
                     if next_occurance.date() == today.date():
                         # td = today.replace(second=0, microsecond=0).time()
+                        no = (next_occurance - timedelta(minutes=15)).time()
                         client.send_message(
                             {
                                 "type": "stream",
                                 "to": "test",
                                 "topic": meet,
-                                "content": f"{next_occurance-timedelta(minutes=15)}",
+                                "content": f"{no}",
                             }
                         )
 
