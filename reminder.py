c384cab10149e698a5507c6cb191b3cc48f99813
adrien5054 <ad.stucki@free.fr>
2023-06-20 15:20:22 +0200

    reminder: create automatic function
    * Test of auto daily notifications

--- reminder.py
+++ reminder.py
@@ -127,7 +127,7 @@ class Reminder(BotPlugin):
                                 "type": "stream",
                                 "to": "test",
                                 "topic": meet,
-                                "content": f"{no_minus_5, no_minus_15, to_time}",
+                                "content": f"{to_time-no_minus_15}",
                             }
                         )
 
