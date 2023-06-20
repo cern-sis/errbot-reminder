11d1fb3657a714ec765e682612c276d3d313c7ea
adrien5054 <ad.stucki@free.fr>
2023-06-20 13:28:48 +0200

    reminder: create automatic function
    * Verification of the message destination

--- reminder.py
+++ reminder.py
@@ -101,10 +101,6 @@ class Reminder(BotPlugin):
             ]
         )
 
-    # def activate(self):
-    #     super().activate()
-    #     self.start_poller(10, self.test_cmd)
-
     @botcmd
     def test_cmd(self, msg, arg):
         client = self._bot.client
@@ -114,6 +110,10 @@ class Reminder(BotPlugin):
                 "type": "stream",
                 "to": "test",
                 "topic": "daily",
-                "text": "TEST Meeting",
+                "content": "TEST Meeting",
             }
         )
+
+    # def activate(self):
+    #     super(Reminder, self).activate()
+    #     self.start_poller(10, self.test_cmd)
