9309e4fcb33cd806051a474f8b691d17add33a8e
adrien5054 <ad.stucki@free.fr>
2023-06-20 15:42:10 +0200

    reminder: create automatic function
    * Solve time conflicts

--- reminder.py
+++ reminder.py
@@ -127,7 +127,7 @@ class Reminder(BotPlugin):
                                 "type": "stream",
                                 "to": "test",
                                 "topic": meet,
-                                "content": f"{type(to_time) == type(no_minus_5)}",
+                                "content": f"{type(to_time) , type(no_minus_5)}",
                             }
                         )
 
