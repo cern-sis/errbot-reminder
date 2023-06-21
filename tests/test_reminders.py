3436f58a47a7dcaaac4f9c09fcc7e4ba06d456a3
adrien5054 <ad.stucki@free.fr>
2023-06-21 08:55:11 +0200

    reminder: create automatic function
    * Send notifications 15 and 5 minutes before every meetings

--- tests/test_reminders.py
+++ tests/test_reminders.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from datetime import date  # , datetime
+from datetime import date, datetime
 
 from freezegun import freeze_time
 
@@ -21,57 +21,57 @@ def test_is_spring_planning():
     assert not Reminder.is_sprint_planning()
 
 
-# @freeze_time("2023-06-12")
-# def test_next_planning():
-#     test_date = datetime(2023, 6, 12, 15, 32)
-#     expected_result = datetime(2023, 6, 19, 15, 30).strftime(
-#         "**%Y-%m-%d** at **%H:%M**"
-#     )
+@freeze_time("2023-06-12")
+def test_next_planning():
+    test_date = datetime(2023, 6, 12, 15, 32)
+    expected_result = datetime(2023, 6, 19, 15, 30).strftime(
+        "**%Y-%m-%d** at **%H:%M**"
+    )
 
-#     assert Reminder.next_occurance("sprint planning", test_date) == expected_result
-
-
-# @freeze_time("2023-06-12")
-# def test_next_review():
-#     test_date = datetime(2023, 6, 12, 15, 32)
-#     expected_result = datetime(2023, 6, 15, 14, 45).strftime(
-#         "**%Y-%m-%d** at **%H:%M**"
-#     )
-
-#     assert Reminder.next_occurance("review", test_date) == expected_result
+    assert Reminder.next_occurance("sprint planning", test_date) == expected_result
 
 
-# @freeze_time("2023-06-12")
-# def test_next_retrospective():
-#     test_date = datetime(2023, 6, 12, 15, 32)
-# expected_result = datetime(2023, 6, 16, 9, 30).strftime("**%Y-%m-%d** at **%H:%M**")
+@freeze_time("2023-06-12")
+def test_next_review():
+    test_date = datetime(2023, 6, 12, 15, 32)
+    expected_result = datetime(2023, 6, 15, 14, 45).strftime(
+        "**%Y-%m-%d** at **%H:%M**"
+    )
 
-#     assert Reminder.next_occurance("Retrospective", test_date) == expected_result
-
-
-# @freeze_time("2023-06-12 15:32", tz_offset=2)
-# def test_next_daily():
-#     test_date = datetime(2023, 6, 12, 15, 32)
-
-# expected_result = datetime(2023, 6, 13, 9, 30).strftime("**%Y-%m-%d** at **%H:%M**")
-
-#     assert Reminder.next_daily(test_date) == expected_result
+    assert Reminder.next_occurance("review", test_date) == expected_result
 
 
-# @freeze_time("2023-06-12")
-# def test_reminder_next():
-#     planning_content = datetime(2023, 6, 19, 15, 30)
-#     daily_content = datetime(2023, 6, 13, 9, 30)
-#     review_content = datetime(2023, 6, 15, 14, 45)
-#     restrospective_content = datetime(2023, 6, 16, 9, 30).strftime(
-#         "**%Y-%m-%d** at **%H:%M**"
-#     )
-#     expected_response = "\n".join(
-#         [
-#    f"Next planning: {planning_content.strftime('**%Y-%m-%d** at **%H:%M**')}",
-#             f"Next daily: {daily_content.strftime('**%Y-%m-%d** at **%H:%M**')}",
-#             f"Next review: {review_content.strftime('**%Y-%m-%d** at **%H:%M**')}",
-#             f"Next retrospective: {restrospective_content}",
-#         ]
-#     )
-#     assert Reminder.reminder_next(None, None, None) == expected_response
+@freeze_time("2023-06-12")
+def test_next_retrospective():
+    test_date = datetime(2023, 6, 12, 15, 32)
+    expected_result = datetime(2023, 6, 16, 9, 30).strftime("**%Y-%m-%d** at **%H:%M**")
+
+    assert Reminder.next_occurance("Retrospective", test_date) == expected_result
+
+
+@freeze_time("2023-06-12 15:32", tz_offset=2)
+def test_next_daily():
+    test_date = datetime(2023, 6, 12, 15, 32)
+
+    expected_result = datetime(2023, 6, 13, 9, 30).strftime("**%Y-%m-%d** at **%H:%M**")
+
+    assert Reminder.next_daily(test_date) == expected_result
+
+
+@freeze_time("2023-06-12")
+def test_reminder_next():
+    planning_content = datetime(2023, 6, 19, 15, 30)
+    daily_content = datetime(2023, 6, 13, 9, 30)
+    review_content = datetime(2023, 6, 15, 14, 45)
+    restrospective_content = datetime(2023, 6, 16, 9, 30).strftime(
+        "**%Y-%m-%d** at **%H:%M**"
+    )
+    expected_response = "\n".join(
+        [
+            f"Next planning: {planning_content.strftime('**%Y-%m-%d** at **%H:%M**')}",
+            f"Next daily: {daily_content.strftime('**%Y-%m-%d** at **%H:%M**')}",
+            f"Next review: {review_content.strftime('**%Y-%m-%d** at **%H:%M**')}",
+            f"Next retrospective: {restrospective_content}",
+        ]
+    )
+    assert Reminder.reminder_next(None, None, None) == expected_response
